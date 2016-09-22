15f4f6b9b2e9ebfc5c66e0c1171fc92294f29a80
Stephan Bakker <info@stephanbakker.nl>
2016-09-22 19:58:06 +0200

    add 'en' on localizedRoutes for simplicity sake, don't bother about base being english

--- src/app/routes.service.ts
+++ src/app/routes.service.ts
@@ -1,42 +1,44 @@
 const localizedRoutes = {
   trips : {
-    nl: 'reizen'
+    nl: 'reizen',
+    en: 'trips',
+    fr: 'voyages'
   },
   profile: {
-    nl: 'profiel'
+    nl: 'profiel',
+    en: 'profile',
+    fr: 'profil'
   }
 };
 
 class RoutesService {
   constructor(private localizedRoutes) {}
 
+  private skipReg = /^(\*\*)?$/;
+
   public translate(baseRoutes, lang) {
-    // if language is baseLanguage, then just return it
-    if (lang === 'en') {
-      return baseRoutes;
-    }
 
     const localized = baseRoutes.map(route => {
       // make sure to return fresh object
       const copy = Object.assign({}, route);
-      if (copy.path === '') {
+      if (this.skipReg.test(copy.path)) {
         return copy;
       }
       copy.path = this.localizedRoutes[route.path][lang];
       return copy;
     });
 
-    // possibily add redirect routes, from base to lang
+    // We could somehow add redirect routes, from base to lang
     // not sure if that makes sense
     // maybe better if some unique ID (e.g. base.path) stays in each route
     // to know where to redirect to
     const base = baseRoutes.reduce((acc, route) => {
       const copy = Object.assign({}, {path: route.path});
-      if (copy.path === '') {
+      if (this.skipReg.test(copy.path)) {
         return acc;
       }
       copy.redirectTo = localizedRoutes[route.path][lang];
-      
+
       acc.push(copy);
       return acc;
     }, []);
